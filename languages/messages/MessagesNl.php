flbf/mediawiki-trunk-phase3

commit 6e7c2dd2bb9bed9583591d8d2c6ecb1470565cd8

Update by Siebrand.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@16900 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

languages/messages/MessagesNl.php
@@ -1035,11 +1035,11 @@ De lengte van de [http://meta.wikimedia.org/wiki/Help:Job_queue job queue] is \'
 'watchlistfor'          => "(voor '''$1''')",
 'nowatchlist'           => 'Uw volglijst is leeg.',
 'watchlistanontext'     => '$1 is verplicht om uw volglijst in te zien of te wijzigen.',
-'watchlistcount'        => "'''Uw volglijst bevat $1 pagina's, inclusief overlegpagina's.'''",
+'watchlistcount'        => "'''Uw volglijst bevat {{PLURAL:$1|één pagina|$1 pagina's}}, inclusief overlegpagina's.'''",
 'clearwatchlist'        => 'Wis volglijst',
 'watchlistcleartext'    => 'Weet u zeker dat u ze wilt verwijderen?',
 'watchlistclearbutton'  => 'Wis volglijst',
-'watchlistcleardone'    => 'Uw volglijst is gewist ($1 pagina\'s verwijderd).',
+'watchlistcleardone'    => "Uw volglijst is gewist. Er {{PLURAL:$1|is één pagina|zijn $1 pagina's}} verwijderd).',
 'watchnologin'          => 'U bent niet aangemeld',
 'watchnologintext'      => 'U dient [[Special:Userlogin|aangemeld]] te zijn om uw volglijst te bewerken.',
 'addedwatch'            => 'Toegevoegd aan volglijst',
@@ -1055,7 +1055,7 @@ Indien u een pagina niet langer wilt volgen, ga dan naar de pagina en klik op "N
 'unwatchthispage'       => 'Niet meer volgen',
 'notanarticle'          => 'Is geen pagina',
 'watchnochange'         => 'Geen van de pagina\'s op uw volglijst is in deze periode bewerkt.',
-'watchdetails'          => '* Er staan $1 pagina\'s op uw volglijst, exclusief overlegpagina\'s
+'watchdetails'          => '* Er {{PLURAL:$1|staat één pagina|staan $1 pagina's}} op uw volglijst, exclusief overlegpagina\'s
 * [[Special:Watchlist/edit|Toon en bewerk de volledige volglijst]]
 * [[Special:Watchlist/clear|Verwijder alle pagina\'s van de volglijst]]',
 'wlheader-enotif'       => '* U wordt per e-mail gewaarschuwd',

commit 051329b3bbc555903ea013fbe6ee82bade436849

Fix PHP parse errors introduced in r16900 by rotemliss Please don't commit things that don't compile!
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@16906 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

languages/messages/MessagesNl.php
@@ -1039,7 +1039,7 @@ De lengte van de [http://meta.wikimedia.org/wiki/Help:Job_queue job queue] is \'
 'clearwatchlist'        => 'Wis volglijst',
 'watchlistcleartext'    => 'Weet u zeker dat u ze wilt verwijderen?',
 'watchlistclearbutton'  => 'Wis volglijst',
-'watchlistcleardone'    => "Uw volglijst is gewist. Er {{PLURAL:$1|is één pagina|zijn $1 pagina's}} verwijderd).',
+'watchlistcleardone'    => "Uw volglijst is gewist. Er {{PLURAL:$1|is één pagina|zijn $1 pagina's}} verwijderd).",
 'watchnologin'          => 'U bent niet aangemeld',
 'watchnologintext'      => 'U dient [[Special:Userlogin|aangemeld]] te zijn om uw volglijst te bewerken.',
 'addedwatch'            => 'Toegevoegd aan volglijst',
@@ -1055,7 +1055,7 @@ Indien u een pagina niet langer wilt volgen, ga dan naar de pagina en klik op "N
 'unwatchthispage'       => 'Niet meer volgen',
 'notanarticle'          => 'Is geen pagina',
 'watchnochange'         => 'Geen van de pagina\'s op uw volglijst is in deze periode bewerkt.',
-'watchdetails'          => '* Er {{PLURAL:$1|staat één pagina|staan $1 pagina's}} op uw volglijst, exclusief overlegpagina\'s
+'watchdetails'          => '* Er {{PLURAL:$1|staat één pagina|staan $1 pagina\'s}} op uw volglijst, exclusief overlegpagina\'s
 * [[Special:Watchlist/edit|Toon en bewerk de volledige volglijst]]
 * [[Special:Watchlist/clear|Verwijder alle pagina\'s van de volglijst]]',
 'wlheader-enotif'       => '* U wordt per e-mail gewaarschuwd',