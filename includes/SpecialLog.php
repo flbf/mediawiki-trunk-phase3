flbf/mediawiki-trunk-phase3

commit 03d9aa3b658a4a3a65eeb3fbdf2fb4118033f082

*Fix bug causing (revert) links not to show for moves
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@20473 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/SpecialLog.php
@@ -585,12 +585,12 @@ class LogViewer {
 		if ( $s->log_type == 'move' && isset( $paramArray[0] ) ) {
 			$destTitle = Title::newFromText( $paramArray[0] );
 			if ( $destTitle ) {
-				$revert = '(' . $this->skin->makeKnownLinkObj( SpecialPage::getTitleFor( 'Movepage' ),
+				$reviewlink = $this->skin->makeKnownLinkObj( SpecialPage::getTitleFor( 'Movepage' ),
 					wfMsg( 'revertmove' ),
 					'wpOldTitle=' . urlencode( $destTitle->getPrefixedDBkey() ) .
 					'&wpNewTitle=' . urlencode( $title->getPrefixedDBkey() ) .
 					'&wpReason=' . urlencode( wfMsgForContent( 'revertmove' ) ) .
-					'&wpMovetalk=0' ) . ')';
+					'&wpMovetalk=0' );
 			}
 		// show undelete link
 		} else if ( $s->log_action == 'delete' && $wgUser->isAllowed( 'delete' ) ) {

commit 1342e095517cd5e9019657e0a8a4e9e744b1669f

Fixing a regression (caused by r20473 and then r20525) that the move revert link was not shown.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@20779 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/SpecialLog.php
@@ -347,12 +347,12 @@ class LogViewer {
 		if ( $s->log_type == 'move' && isset( $paramArray[0] ) ) {
 			$destTitle = Title::newFromText( $paramArray[0] );
 			if ( $destTitle ) {
-				$reviewlink = $this->skin->makeKnownLinkObj( SpecialPage::getTitleFor( 'Movepage' ),
+				$revert = '(' . $this->skin->makeKnownLinkObj( SpecialPage::getTitleFor( 'Movepage' ),
 					wfMsg( 'revertmove' ),
 					'wpOldTitle=' . urlencode( $destTitle->getPrefixedDBkey() ) .
 					'&wpNewTitle=' . urlencode( $title->getPrefixedDBkey() ) .
 					'&wpReason=' . urlencode( wfMsgForContent( 'revertmove' ) ) .
-					'&wpMovetalk=0' );
+					'&wpMovetalk=0' ) . ')';
 			}
 		// show undelete link
 		} elseif ( $s->log_action == 'delete' && $wgUser->isAllowed( 'delete' ) ) {