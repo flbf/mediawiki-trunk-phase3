flbf/mediawiki-trunk-phase3

commit 900379d9c48e9a166b5dadaf1c58753c2aa7f812

Redo r41723: still show rcnotefrom as needed, but not rcnote. Add rclegend message if it is not empty.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@42769 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/specials/SpecialRecentchanges.php
@@ -595,17 +595,15 @@ class SpecialRecentChanges extends SpecialPage {
 
 		$options = $nondefaults + $defaults;
 
-		if( $options['from'] )
-			$note = wfMsgExt( 'rcnotefrom', array( 'parseinline' ),
+		$note = '';
+		if( $options['from'] ) {
+			$note .= wfMsgExt( 'rcnotefrom', array( 'parseinline' ),
 				$wgLang->formatNum( $options['limit'] ),
-				$wgLang->timeanddate( $options['from'], true ) );
-		else
-			$note = wfMsgExt( 'rcnote', array( 'parseinline' ),
-				$wgLang->formatNum( $options['limit'] ),
-				$wgLang->formatNum( $options['days'] ),
-				$wgLang->timeAndDate( wfTimestampNow(), true ),
-				$wgLang->date( wfTimestampNow(), true ),
-				$wgLang->time( wfTimestampNow(), true ) );
+				$wgLang->timeanddate( $options['from'], true ) ) . '<br />';
+		}
+		if( !wfEmptyMsg( 'rclegend', wfMsg('rclegend') ) ) {
+			$note .= wfMsgExt( 'rclegend', array('parseinline') ) . '<br />';
+		}
 
 		# Sort data for display and make sure it's unique after we've added user data.
 		$wgRCLinkLimits[] = $options['limit'];
@@ -661,6 +659,6 @@ class SpecialRecentChanges extends SpecialPage {
 		$rclinks = wfMsgExt( 'rclinks', array( 'parseinline', 'replaceafter' ),
 			$cl, $dl, $hl );
 		$rclistfrom = wfMsgExt( 'rclistfrom', array( 'parseinline', 'replaceafter' ), $tl );
-		return "$note<br />$rclinks<br />$rclistfrom";
+		return "{$note}$rclinks<br />$rclistfrom";
 	}
 }

commit 004889b7879844f3d4320bc8264bfc50e3b3a949

* fixing regression from r42769 (component ordering & empty line)
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@46173 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/specials/SpecialRecentchanges.php
@@ -588,13 +588,13 @@ class SpecialRecentChanges extends SpecialPage {
 		$options = $nondefaults + $defaults;
 
 		$note = '';
+		if( !wfEmptyMsg( 'rclegend', wfMsg('rclegend') ) ) {
+			$note .= wfMsgExt( 'rclegend', array('parseinline') );
+		}
 		if( $options['from'] ) {
 			$note .= wfMsgExt( 'rcnotefrom', array( 'parseinline' ),
 				$wgLang->formatNum( $options['limit'] ),
 				$wgLang->timeanddate( $options['from'], true ) ) . '<br />';
-		}
-		if( !wfEmptyMsg( 'rclegend', wfMsg('rclegend') ) ) {
-			$note .= wfMsgExt( 'rclegend', array('parseinline') ) . '<br />';
 		}
 
 		# Sort data for display and make sure it's unique after we've added user data.