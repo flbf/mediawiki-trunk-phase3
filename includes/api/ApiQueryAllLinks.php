flbf/mediawiki-trunk-phase3

commit b53893a9060b981cf799969e60f99166ba94db78

* API: BREAKING CHANGE: (bug 11430) Return fewer results than the limit in some cases to prevent running out of memory * This means queries could possibly return fewer results than the limit and still set a query-continue * Add iicontinue, rvcontinue, cicontinue, incontinue, amfrom to faciliate query-continue for these modules * Implemented by blocking additions to the ApiResult object if they would make it too large ** Important things like query-continue values and warnings are exempt from this check ** RSS feeds and exported XML are also exempted (size-checking them would be too messy) ** Result size is checked against $wgAPIMaxResultSize, which defaults to 8 MB
For those who really care, per-file details follow:

ApiResult.php:
* Introduced ApiResult::$mSize which keeps track of the result size.
* Introduced ApiResult::size() which calculates an array's size
  (which is the sum of the strlen()s of its elements).
* ApiResult::addValue() now checks that the result size stays below
  $wgAPIMaxResultSize. If the item won't fit, it won't be added and addValue()
  will return false. Callers should check the return value and set a
  query-continue if it's false.
* Closed the back door that is ApiResult::getData(): callers can't manipulate
  the data array directly anymore so they can't bypass the result size limit.
* Added ApiResult::setIndexedTagName_internal() which will call
  setIndexedTagName() on an array already in the result. This is needed for the
  'new' order of adding results, which means addValue()ing one result at a time
  until you hit the limit or run out, then calling this function to set the tag
  name.
* Added ApiResult::disableSizeCheck() and enableSizeCheck() which disable and
  enable size checking in addValue(). This is used for stuff like query-continue
  elements and warnings which shouldn't count towards the result size.
* Added ApiResult::unsetValue() which removes an element from the result and
  decreases $mSize.

ApiBase.php:

includes/api/ApiQueryAllLinks.php
@@ -101,8 +101,9 @@ class ApiQueryAllLinks extends ApiQueryGeneratorBase {
 
 		$res = $this->select(__METHOD__);
 
-		$data = array ();
+		$pageids = array ();
 		$count = 0;
+		$result = $this->getResult();
 		while ($row = $db->fetchObject($res)) {
 			if (++ $count > $limit) {
 				// We've reached the one extra which shows that there are additional pages to be had. Stop here...
@@ -123,7 +124,13 @@ class ApiQueryAllLinks extends ApiQueryGeneratorBase {
 					$vals['ns'] = intval($title->getNamespace());
 					$vals['title'] = $title->getPrefixedText();
 				}
-				$data[] = $vals;
+				$fit = $result->addValue(array('query', $this->getModuleName()), null, $vals);
+				if(!$fit)
+				{
+					$this->setContinueEnumParameter('continue',
+						$this->keyToTitle($row->pl_title) . "|" . $row->pl_from);
+					break;
+				}
 			} else {
 				$pageids[] = $row->pl_from;
 			}
@@ -131,9 +138,7 @@ class ApiQueryAllLinks extends ApiQueryGeneratorBase {
 		$db->freeResult($res);
 
 		if (is_null($resultPageSet)) {
-			$result = $this->getResult();
-			$result->setIndexedTagName($data, 'l');
-			$result->addValue('query', $this->getModuleName(), $data);
+			$result->setIndexedTagName_internal(array('query', $this->getModuleName()), 'l');
 		} else {
 			$resultPageSet->populateFromPageIDs($pageids);
 		}

includes/api/ApiQueryAllmessages.php
@@ -75,6 +75,9 @@ class ApiQueryAllmessages extends ApiQueryBase {
 		//Get all requested messages
 		$messages = array();
 		foreach( $messages_target as $message ) {
+			if(!is_null($params['from']))
+				if($message < $params['from'])
+					continue;
 			$messages[$message] = wfMsg( $message );
 		}
 
@@ -89,10 +92,14 @@ class ApiQueryAllmessages extends ApiQueryBase {
 			} else {
 				$result->setContent( $message, $value );
 			}
-			$messages_out[] = $message;
+			$fit = $result->addValue(array('query', $this->getModuleName()), null, $message);
+			if(!$fit)
+			{
+				$this->setContinueEnumParameter('from', $name);
+				break;
+			}
 		}
-		$result->setIndexedTagName( $messages_out, 'message' );
-		$result->addValue( 'query', $this->getModuleName(), $messages_out );
+		$result->setIndexedTagName_internal(array('query', $this->getModuleName()), 'message');
 	}
 
 	public function getAllowedParams() {
@@ -102,6 +109,7 @@ class ApiQueryAllmessages extends ApiQueryBase {
 			),
 			'filter' => array(),
 			'lang' => null,
+			'from' => null,
 		);
 	}
 
@@ -110,6 +118,7 @@ class ApiQueryAllmessages extends ApiQueryBase {
 			'messages' => 'Which messages to output. "*" means all messages',
 			'filter' => 'Return only messages that contain this string',
 			'lang' => 'Return messages in this language',
+			'from' => 'Return messages starting at this message',
 		);
 	}
 

includes/api/ApiQueryDeletedrevs.php
@@ -173,9 +173,9 @@ class ApiQueryDeletedrevs extends ApiQueryBase {
 			$this->addWhereRange('ar_timestamp', $params['dir'], $params['start'], $params['end']);
 		}
 		$res = $this->select(__METHOD__);
-		$pages = array();
+		$pageMap = array(); // Maps ns&title to (fake) pageid
 		$count = 0;
-		// First populate the $pages array
+		$newPageID = 0;
 		while($row = $db->fetchObject($res))
 		{
 			if(++$count > $limit)
@@ -205,31 +205,37 @@ class ApiQueryDeletedrevs extends ApiQueryBase {
 			if($fld_content)
 				ApiResult::setContent($rev, Revision::getRevisionText($row));
 
-			$t = Title::makeTitle($row->ar_namespace, $row->ar_title);
-			if(!isset($pages[$t->getPrefixedText()]))
+			if(!isset($pageMap[$row->ar_namespace][$row->ar_title]))
 			{
-				$pages[$t->getPrefixedText()] = array(
+				$pageID = $newPageID++;
+				$pageMap[$row->ar_namespace][$row->ar_title] = $pageID;
+				$t = Title::makeTitle($row->ar_namespace, $row->ar_title);
+				$a = array(
 					'title' => $t->getPrefixedText(),
 					'ns' => intval($row->ar_namespace),
 					'revisions' => array($rev)
 				);
+				$result->setIndexedTagName($a['revisions'], 'rev');
 				if($fld_token)
-					$pages[$t->getPrefixedText()]['token'] = $token;
+					$a['token'] = $token;
+				$fit = $result->addValue(array('query', $this->getModuleName()), $pageID, $a);
 			}
 			else
-				$pages[$t->getPrefixedText()]['revisions'][] = $rev;
+			{
+				$pageID = $pageMap[$row->ar_namespace][$row->ar_title];
+				$fit = $result->addValue(
+					array('query', $this->getModuleName(), $pageID, 'revisions'),
+					null, $rev);
+			}
+			if(!$fit)
+			{
+				$this->setContinueEnumParameter('start', wfTimestamp(TS_ISO_8601, $row->ar_timestamp));
+				break;
+			}
 		}
 		$db->freeResult($res);
-
-		// We don't want entire pagenames as keys, so let's make this array indexed
-		foreach($pages as $page)
-		{
-			$result->setIndexedTagName($page['revisions'], 'rev');
-			$data[] = $page;
-		}
-		$result->setIndexedTagName($data, 'page');
-		$result->addValue('query', $this->getModuleName(), $data);
-		}
+		$result->setIndexedTagName_internal(array('query', $this->getModuleName()), 'page');
+	}
 
 	public function getAllowedParams() {
 		return array (

includes/api/ApiQueryRevisions.php
@@ -231,6 +231,8 @@ class ApiQueryRevisions extends ApiQueryBase {
 			ApiBase :: dieDebug(__METHOD__, 'param validation?');
 
 		$this->addOption('LIMIT', $limit +1);
+		if(!is_null($continue))
+			$this->addOption('OFFSET', $continue);
 
 		$data = array ();
 		$count = 0;
@@ -246,29 +248,19 @@ class ApiQueryRevisions extends ApiQueryBase {
 				$this->setContinueEnumParameter('startid', intval($row->rev_id));
 				break;
 			}
-
 			$revision = new Revision( $row );
-			$this->getResult()->addValue(
-				array (
-					'query',
-					'pages',
-					$revision->getPage(),
-					'revisions'),
-				null,
-				$this->extractRowInfo( $revision ));
-		}
-		$db->freeResult($res);
-
-		// Ensure that all revisions are shown as '<rev>' elements
-		$result = $this->getResult();
-		if ($result->getIsRawMode()) {
-			$data =& $result->getData();
-			foreach ($data['query']['pages'] as & $page) {
-				if (is_array($page) && array_key_exists('revisions', $page)) {
-					$result->setIndexedTagName($page['revisions'], 'rev');
-				}
+			//
+			$fit = $this->addPageSubItem($revision->getPage(), $this->extractRowInfo($revision), 'rev');
+			if(!$fit)
+			{
+				if($enumRevMode)
+					$this->setContinueEnumParameter('startid', intval($row->rev_id));
+				else
+					$this->setContinueEnumParameter('continue', $continue + $count - 1);
+				break;
 			}
 		}
+		$db->freeResult($res);
 	}
 
 	private function extractRowInfo( $revision ) {
@@ -412,6 +404,7 @@ class ApiQueryRevisions extends ApiQueryBase {
 				ApiBase :: PARAM_TYPE => array_keys($this->getTokenFunctions()),
 				ApiBase :: PARAM_ISMULTI => true
 			),
+			'continue' => null,
 		);
 	}
 
@@ -430,6 +423,7 @@ class ApiQueryRevisions extends ApiQueryBase {
 			'generatexml' => 'generate XML parse tree for revision content',
 			'section' => 'only retrieve the content of this section',
 			'token' => 'Which tokens to obtain for each revision',
+			'continue' => 'When more results are available, use this to continue',
 		);
 	}
 

includes/DefaultSettings.php
@@ -3519,6 +3519,12 @@ $wgAPIListModules = array();
 $wgAPIMaxDBRows = 5000;
 
 /**
+ * The maximum size (in bytes) of an API result.
+ * Don't set this lower than $wgMaxArticleSize*1024
+ */
+$wgAPIMaxResultSize = 8388608;
+
+/**
  * Parser test suite files to be run by parserTests.php when no specific
  * filename is passed to it.
  *

includes/api/ApiBase.php
@@ -147,7 +147,7 @@ abstract class ApiBase {
 	/**
 	 * Get the result data array
 	 */
-	public function & getResultData() {
+	public function getResultData() {
 		return $this->getResult()->getData();
 	}
 
@@ -156,20 +156,23 @@ abstract class ApiBase {
 	 * notice any changes in API.
 	 */
 	public function setWarning($warning) {
-		# If there is a warning already, append it to the existing one
-		$data =& $this->getResult()->getData();
+		$data = $this->getResult()->getData();
 		if(isset($data['warnings'][$this->getModuleName()]))
 		{
 			# Don't add duplicate warnings
 			$warn_regex = preg_quote($warning, '/');
 			if(preg_match("/{$warn_regex}(\\n|$)/", $data['warnings'][$this->getModuleName()]['*']))
 				return;
-			$warning = "{$data['warnings'][$this->getModuleName()]['*']}\n$warning";
-			unset($data['warnings'][$this->getModuleName()]);
+			$oldwarning = $data['warnings'][$this->getModuleName()]['*'];
+			# If there is a warning already, append it to the existing one
+			$warning = "$oldwarning\n$warning";
+			$this->getResult()->unsetValue('warnings', $this->getModuleName());
 		}
 		$msg = array();
 		ApiResult :: setContent($msg, $warning);
+		$this->getResult()->disableSizeCheck();
 		$this->getResult()->addValue('warnings', $this->getModuleName(), $msg);
+		$this->getResult()->enableSizeCheck();
 	}
 
 	/**

includes/api/ApiFormatBase.php
@@ -261,9 +261,13 @@ class ApiFormatFeedWrapper extends ApiFormatBase {
 	public static function setResult($result, $feed, $feedItems) {
 		// Store output in the Result data.
 		// This way we can check during execution if any error has occured
-		$data = & $result->getData();
-		$data['_feed'] = $feed;
-		$data['_feeditems'] = $feedItems;
+		// Disable size checking for this because we can't continue
+		// cleanly; size checking would cause more problems than it'd
+		// solve
+		$result->disableSizeCheck();
+		$result->addValue(null, '_feed', $feed);
+		$result->addValue(null, '_feeditems', $feedItems);
+		$result->enableSizeCheck();
 	}
 
 	/**
@@ -282,8 +286,8 @@ class ApiFormatFeedWrapper extends ApiFormatBase {
 
 	/**
 	 * This class expects the result data to be in a custom format set by self::setResult()
-	 * $result['_feed']		 - an instance of one of the $wgFeedClasses classes
-	 * $result['_feeditems'] - an array of FeedItem instances
+	 * $result['_feed']		- an instance of one of the $wgFeedClasses classes
+	 * $result['_feeditems']	- an array of FeedItem instances
 	 */
 	public function execute() {
 		$data = $this->getResultData();

includes/api/ApiMain.php
@@ -361,9 +361,11 @@ class ApiMain extends ApiBase {
 			}
 
 			$this->getResult()->reset();
+			$this->getResult()->disableSizeCheck();
 			// Re-add the id
-			if($this->mRequest->getCheck('requestid'))
-				$this->getResult()->addValue(null, 'requestid', $this->mRequest->getVal('requestid'));
+			$requestid = $this->getParameter('requestid');
+			if(!is_null($requestid))
+				$this->getResult()->addValue(null, 'requestid', $requestid);
 			$this->getResult()->addValue(null, 'error', $errMessage);
 
 		return $errMessage['code'];
@@ -374,8 +376,9 @@ class ApiMain extends ApiBase {
 	 */
 	protected function executeAction() {
 		// First add the id to the top element
-		if($this->mRequest->getCheck('requestid'))
-			$this->getResult()->addValue(null, 'requestid', $this->mRequest->getVal('requestid'));
+		$requestid = $this->getParameter('requestid');
+		if(!is_null($requestid))
+			$this->getResult()->addValue(null, 'requestid', $requestid);
 
 		$params = $this->extractRequestParams();
 

commit 044d0b675902fce3612183c55c875618e124e98e

Fix some more regressions from r46845 reported by Brad Jorsch on the mediawiki-api list <http://lists.wikimedia.org/pipermail/mediawiki-api/2009-February/000924.html>
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@47036 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/api/ApiQueryAllLinks.php
@@ -127,8 +127,10 @@ class ApiQueryAllLinks extends ApiQueryGeneratorBase {
 				$fit = $result->addValue(array('query', $this->getModuleName()), null, $vals);
 				if(!$fit)
 				{
-					$this->setContinueEnumParameter('continue',
-						$this->keyToTitle($row->pl_title) . "|" . $row->pl_from);
+					if($params['unique'])
+						$this->setContinueEnumParameter('from', $this->keyToTitle($row->pl_title));
+					else
+						$this->setContinueEnumParameter('continue', $this->keyToTitle($row->pl_title) . "|" . $row->pl_from);
 					break;
 				}
 			} else {

includes/api/ApiQueryAllmessages.php
@@ -76,7 +76,7 @@ class ApiQueryAllmessages extends ApiQueryBase {
 		$messages = array();
 		foreach( $messages_target as $message ) {
 			if(!is_null($params['from']))
-				if($message < $params['from'])
+				if($message != $params['from'])
 					continue;
 			$messages[$message] = wfMsg( $message );
 		}

includes/api/ApiQueryDeletedrevs.php
@@ -229,7 +229,11 @@ class ApiQueryDeletedrevs extends ApiQueryBase {
 			}
 			if(!$fit)
 			{
-				$this->setContinueEnumParameter('start', wfTimestamp(TS_ISO_8601, $row->ar_timestamp));
+				if($mode == 'all')
+					$this->setContinueEnumParameter('continue', $this->keyToTitle($row->ar_title) . '|' .
+						$row->ar_timestamp);
+				else
+					$this->setContinueEnumParameter('start', wfTimestamp(TS_ISO_8601, $row->ar_timestamp));
 				break;
 			}
 		}

includes/api/ApiQueryRevisions.php
@@ -231,8 +231,8 @@ class ApiQueryRevisions extends ApiQueryBase {
 			ApiBase :: dieDebug(__METHOD__, 'param validation?');
 
 		$this->addOption('LIMIT', $limit +1);
-		if(!is_null($continue))
-			$this->addOption('OFFSET', $continue);
+		if(!is_null($params['continue']))
+			$this->addOption('OFFSET', $params['continue']);
 
 		$data = array ();
 		$count = 0;
@@ -256,7 +256,7 @@ class ApiQueryRevisions extends ApiQueryBase {
 				if($enumRevMode)
 					$this->setContinueEnumParameter('startid', intval($row->rev_id));
 				else
-					$this->setContinueEnumParameter('continue', $continue + $count - 1);
+					$this->setContinueEnumParameter('continue', $params['continue'] + $count - 1);
 				break;
 			}
 		}