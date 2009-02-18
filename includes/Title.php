flbf/mediawiki-trunk-phase3

commit 427b15b37898bdd6284f4c9ec4e3289e21542c4d

General cleanup of SQL in Title and File. Fixes bug 17392: 'FOR UPDATE' should be passed in an array, not as a string.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@47374 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/Title.php
@@ -2418,13 +2418,13 @@ class Title {
 	 * WARNING: do not use this function on arbitrary user-supplied titles!
 	 * On heavily-used templates it will max out the memory.
 	 *
-	 * @param $options \type{\string} may be FOR UPDATE
+	 * @param array $options may be FOR UPDATE
 	 * @return \type{\arrayof{Title}} the Title objects linking here
 	 */
-	public function getLinksTo( $options = '', $table = 'pagelinks', $prefix = 'pl' ) {
+	public function getLinksTo( $options = array(), $table = 'pagelinks', $prefix = 'pl' ) {
 		$linkCache = LinkCache::singleton();
 
-		if ( $options ) {
+		if ( count( $options ) > 0 ) {
 			$db = wfGetDB( DB_MASTER );
 		} else {
 			$db = wfGetDB( DB_SLAVE );
@@ -2459,10 +2459,10 @@ class Title {
 	 * WARNING: do not use this function on arbitrary user-supplied titles!
 	 * On heavily-used templates it will max out the memory.
 	 *
-	 * @param $options \type{\string} may be FOR UPDATE
+	 * @param array $options may be FOR UPDATE
 	 * @return \type{\arrayof{Title}} the Title objects linking here
 	 */
-	public function getTemplateLinksTo( $options = '' ) {
+	public function getTemplateLinksTo( $options = array() ) {
 		return $this->getLinksTo( $options, 'templatelinks', 'tl' );
 	}
 
@@ -2470,16 +2470,16 @@ class Title {
 	 * Get an array of Title objects referring to non-existent articles linked from this page
 	 *
 	 * @todo check if needed (used only in SpecialBrokenRedirects.php, and should use redirect table in this case)
-	 * @param $options \type{\string} may be FOR UPDATE
+	 * @param array $options may be FOR UPDATE
 	 * @return \type{\arrayof{Title}} the Title objects
 	 */
-	public function getBrokenLinksFrom( $options = '' ) {
+	public function getBrokenLinksFrom( $options = array() ) {
 		if ( $this->getArticleId() == 0 ) {
 			# All links from article ID 0 are false positives
 			return array();
 		}
 
-		if ( $options ) {
+		if ( count( $options ) > 0 ) {
 			$db = wfGetDB( DB_MASTER );
 		} else {
 			$db = wfGetDB( DB_SLAVE );
@@ -3067,7 +3067,7 @@ class Title {
 			array( 'page_is_redirect', 'page_latest', 'page_id' ),
 			$this->pageCond(),
 			__METHOD__,
-			'FOR UPDATE'
+			array( 'FOR UPDATE' )
 		);
 		# Cache some fields we may want
 		$this->mArticleID = $row ? intval($row->page_id) : 0;
@@ -3085,7 +3085,7 @@ class Title {
 				'page_latest != rev_id'
 			), 
 			__METHOD__,
-			'FOR UPDATE'
+			array( 'FOR UPDATE' )
 		);
 		# Return true if there was no history
 		return ($row === false);

includes/filerepo/File.php
@@ -865,22 +865,23 @@ abstract class File {
 	 *
 	 * @deprecated Use HTMLCacheUpdate, this function uses too much memory
 	 */
-	function getLinksTo( $options = '' ) {
+	function getLinksTo( $options = array() ) {
 		wfProfileIn( __METHOD__ );
 
 		// Note: use local DB not repo DB, we want to know local links
-		if ( $options ) {
+		if ( count( $options ) > 0 ) {
 			$db = wfGetDB( DB_MASTER );
 		} else {
 			$db = wfGetDB( DB_SLAVE );
 		}
 		$linkCache = LinkCache::singleton();
 
-		list( $page, $imagelinks ) = $db->tableNamesN( 'page', 'imagelinks' );
 		$encName = $db->addQuotes( $this->getName() );
-		$sql = "SELECT page_namespace,page_title,page_id,page_len,page_is_redirect,
-			FROM $page,$imagelinks WHERE page_id=il_from AND il_to=$encName $options";
-		$res = $db->query( $sql, __METHOD__ );
+		$res = $db->select( array( 'page', 'imagelinks'), 
+							array( 'page_namespace', 'page_title', 'page_id', 'page_len', 'page_is_redirect' ),
+							array( 'page_id' => 'il_from', 'il_to' => $encName ),
+							__METHOD__,
+							$options );
 
 		$retVal = array();
 		if ( $db->numRows( $res ) ) {

commit 08301d6d780fbfb0ea7c5b6978946546bf96c8a0

Fixed Title::getBrokenLinksFrom(), totally broken by r47374. Caused breakage of Special:BrokenRedirects on Wikimedia. Removed $options parameter, it was not used anywhere and definitely should never be used anywhere due to the risk of breaking the wiki. Brought the coding style into this century.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@47434 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/Title.php
@@ -2443,42 +2443,35 @@ class Title {
 	 * Get an array of Title objects referring to non-existent articles linked from this page
 	 *
 	 * @todo check if needed (used only in SpecialBrokenRedirects.php, and should use redirect table in this case)
-	 * @param array $options may be FOR UPDATE
 	 * @return \type{\arrayof{Title}} the Title objects
 	 */
-	public function getBrokenLinksFrom( $options = array() ) {
+	public function getBrokenLinksFrom() {
 		if ( $this->getArticleId() == 0 ) {
 			# All links from article ID 0 are false positives
 			return array();
 		}
 
-		if ( count( $options ) > 0 ) {
-			$db = wfGetDB( DB_MASTER );
-		} else {
-			$db = wfGetDB( DB_SLAVE );
-		}
-
-		$res = $db->safeQuery(
-			  "SELECT pl_namespace, pl_title
-			     FROM !
-			LEFT JOIN !
-			       ON pl_namespace=page_namespace
-			      AND pl_title=page_title
-			    WHERE pl_from=?
-			      AND page_namespace IS NULL
-				  !",
-			$db->tableName( 'pagelinks' ),
-			$db->tableName( 'page' ),
-			$this->getArticleId(),
-			$options );
+		$dbr = wfGetDB( DB_SLAVE );
+		$res = $dbr->select(
+			array( 'page', 'pagelinks' ),
+			array( 'pl_namespace', 'pl_title' ),
+			array(
+				'pl_from' => $this->getArticleId(),
+				'page_namespace IS NULL'
+			),
+			__METHOD__, array(),
+			array(
+				'page' => array( 
+					'LEFT JOIN', 
+					array( 'pl_namespace=page_namespace', 'pl_title=page_title' )
+				)
+			)
+		);
 
 		$retVal = array();
-		if ( $db->numRows( $res ) ) {
-			foreach( $res as $row ) {
-				$retVal[] = Title::makeTitle( $row->pl_namespace, $row->pl_title );
-			}
+		foreach( $res as $row ) {
+			$retVal[] = Title::makeTitle( $row->pl_namespace, $row->pl_title );
 		}
-		$db->freeResult( $res );
 		return $retVal;
 	}
 