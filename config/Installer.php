flbf/mediawiki-trunk-phase3

commit cc29409eef237c05f7d99dcab102354ed5c90983

(bug 20260) SQLite: don't always autocreate DB and its directory. If the file isn't readable at execution time, we now throw an exception
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@55669 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

RELEASE-NOTES
@@ -438,6 +438,9 @@ this. Was used when mwEmbed was going to be an extension.
 * (bug 19055) maintenance/rebuildrecentchanges.php now purges
   Special:Recentchanges's RSS and Atom feed cache
 * The installer will now try to bypass PHP's max_execution_time
+* (bug 20260) SQLite no longer tries to automatically create the database at
+  execution time, this now happens only at install time; if it is not available 
+  at script execution, it now throws an exception
 
 == API changes in 1.16 ==
 

includes/db/DatabaseSqlite.php
@@ -21,12 +21,12 @@ class DatabaseSqlite extends DatabaseBase {
 	 * Constructor
 	 */
 	function __construct($server = false, $user = false, $password = false, $dbName = false, $failFunction = false, $flags = 0) {
-		global $wgSQLiteDataDir, $wgSQLiteDataDirMode;
-		if ("$wgSQLiteDataDir" == '') $wgSQLiteDataDir = dirname($_SERVER['DOCUMENT_ROOT']).'/data';
-		if (!is_dir($wgSQLiteDataDir)) wfMkdirParents( $wgSQLiteDataDir, $wgSQLiteDataDirMode );
+		global $wgSQLiteDataDir;
 		$this->mFailFunction = $failFunction;
 		$this->mFlags = $flags;
 		$this->mDatabaseFile = "$wgSQLiteDataDir/$dbName.sqlite";
+		if( !is_readable( $this->mDatabaseFile ) )
+			throw new DBConnectionError( $this, "SQLite database not accessible" );
 		$this->mName = $dbName;
 		$this->open($server, $user, $password, $dbName);
 	}

commit 11a3f186e820a43b8ffc0c8af0b3a9311bb5d6fd

(bug 20911) Installer failed to create a SQLite database, regression from r55669.
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@57959 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

RELEASE-NOTES
@@ -573,6 +573,7 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
 * (bug 20880) Fixed updater failure on SQLite backend
 * (bug 21182) Fixed invalid HTML in Special:Listgrouprights
 * (bug 20242) Installer no longer promts for user credentials for SQLite databases
+* (bug 20911) Installer failed to create a SQLite database
 
 == API changes in 1.16 ==
 

config/Installer.php
@@ -975,8 +975,8 @@ if( $conf->posted && ( 0 == count( $errs ) ) ) {
 			if ("$wgSQLiteDataDir" == '') {
 				$wgSQLiteDataDir = dirname($_SERVER['DOCUMENT_ROOT']).'/data';
 			}
-			echo "<li>Attempting to connect to SQLite database at \"" . 
-				htmlspecialchars( $wgSQLiteDataDir ) .  "\"";
+			echo '<li>Attempting to connect to SQLite database at "' . 
+				htmlspecialchars( $wgSQLiteDataDir ) . '": ';
 			if ( !is_dir( $wgSQLiteDataDir ) ) {
 				if ( is_writable( dirname( $wgSQLiteDataDir ) ) ) {
 					$ok = wfMkdirParents( $wgSQLiteDataDir, $wgSQLiteDataDirMode );
@@ -984,25 +984,40 @@ if( $conf->posted && ( 0 == count( $errs ) ) ) {
 					$ok = false;
 				}
 				if ( !$ok ) {
-					echo ": cannot create data directory</li>";
+					echo "cannot create data directory</li>";
 					$errs['SQLiteDataDir'] = 'Enter a valid data directory';
 					continue;
 				}
 			}
 			if ( !is_writable( $wgSQLiteDataDir ) ) {
-				echo ": data directory not writable</li>";
+				echo "data directory not writable</li>";
 				$errs['SQLiteDataDir'] = 'Enter a writable data directory';
 				continue;
 			}
-			$dataFile = "$wgSQLiteDataDir/$wgDBname.sqlite";
-			if ( file_exists( $dataFile ) && !is_writable( $dataFile ) ) {
-				echo ": data file not writable</li>";
-				$errs['SQLiteDataDir'] = "$wgDBname.sqlite is not writable";
+			$dataFile = DatabaseSqlite::generateFileName( $wgSQLiteDataDir, $wgDBname );
+			if ( file_exists( $dataFile ) ) {
+				if ( !is_writable( $dataFile ) ) {
+					echo "data file not writable</li>";
+					$errs['SQLiteDataDir'] = basename( $dataFile ) . " is not writable";
+					continue;
+				}
+			} else {
+				if ( file_put_contents( $dataFile, '' ) === false ) {
+					echo 'could not create database file "' . htmlspecialchars( basename( $dataFile ) ) . "\"</li>\n";
+					$errs['SQLiteDataDir'] = "couldn't create " . basename( $dataFile );
+					continue;
+				}
+			}
+			try {
+				$wgDatabase = new DatabaseSqlite( false, false, false, $wgDBname, 1 );
+			}
+			catch( MWException $ex ) {
+				echo 'error: ' . htmlspecialchars( $ex->getMessage() ) . "</li>\n";
 				continue;
 			}
-			$wgDatabase = new DatabaseSqlite( false, false, false, $wgDBname, 1 );
+			
 			if (!$wgDatabase->isOpen()) {
-				print ": error: " . htmlspecialchars( $wgDatabase->lastError() ) . "</li>\n";
+				print "error: " . htmlspecialchars( $wgDatabase->lastError() ) . "</li>\n";
 				$errs['SQLiteDataDir'] = 'Could not connect to database';
 				continue;
 			} else {

includes/db/DatabaseSqlite.php
@@ -24,7 +24,7 @@ class DatabaseSqlite extends DatabaseBase {
 		global $wgSQLiteDataDir;
 		$this->mFailFunction = $failFunction;
 		$this->mFlags = $flags;
-		$this->mDatabaseFile = "$wgSQLiteDataDir/$dbName.sqlite";
+		$this->mDatabaseFile = self::generateFileName( $wgSQLiteDataDir, $dbName );
 		if( !is_readable( $this->mDatabaseFile ) )
 			throw new DBConnectionError( $this, "SQLite database not accessible" );
 		$this->mName = $dbName;
@@ -84,6 +84,16 @@ class DatabaseSqlite extends DatabaseBase {
 			$this->mConn = null;
 		}
 		return true;
+	}
+
+	/**
+	 * Generates a database file name. Explicitly public for installer.
+	 * @param $dir String: Directory where database resides
+	 * @param $dbName String: Database name
+	 * @return String
+	 */
+	public static function generateFileName( $dir, $dbName ) {
+		return "$dir/$dbName.sqlite";
 	}
 
 	/**
@@ -420,8 +430,7 @@ class DatabaseSqlite extends DatabaseBase {
 	 * - this is the same way PostgreSQL works, MySQL reads in tables.sql and interwiki.sql using dbsource (which calls db->sourceFile)
 	 */
 	public function setup_database() {
-		global $IP, $wgSQLiteDataDir, $wgDBTableOptions;
-		$wgDBTableOptions = '';
+		global $IP;
 
 		# Process common MySQL/SQLite table definitions
 		$err = $this->sourceFile( "$IP/maintenance/tables.sql" );