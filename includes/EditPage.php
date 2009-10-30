flbf/mediawiki-trunk-phase3

commit ab85afb6442f78dd081001eb046e5c33fe9ec3fd

* (bug 1989) Some messages converted to use addWikiText(), patch by Niklas Laxstr��m
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@8855 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/EditPage.php
@@ -446,7 +446,7 @@ class EditPage {
 		if ( $isConflict ) {
 			$s = wfMsg( 'editconflict', $this->mTitle->getPrefixedText() );
 			$wgOut->setPageTitle( $s );
-			$wgOut->addHTML( wfMsg( 'explainconflict' ) );
+			$wgOut->addWikiText( wfMsg( 'explainconflict' ) );
 
 			$this->textbox2 = $this->textbox1;
 			$this->textbox1 = $this->mArticle->getContent( true );
@@ -477,27 +477,22 @@ class EditPage {
 			}
 			if ( $this->oldid ) {
 				$this->mArticle->setOldSubtitle();
-				$wgOut->addHTML( wfMsg( 'editingold' ) );
+				$wgOut->addWikiText( wfMsg( 'editingold' ) );
 			}
 		}
 
 		if( wfReadOnly() ) {
-			$wgOut->addHTML( '<strong>' .
-			wfMsg( 'readonlywarning' ) .
-			"</strong>" );
+			$wgOut->addWikiText( wfMsg( 'readonlywarning' ) );
 		} else if ( $isCssJsSubpage and 'preview' != $formtype) {
-			$wgOut->addHTML( wfMsg( 'usercssjsyoucanpreview' ));
+			$wgOut->addWikiText( wfMsg( 'usercssjsyoucanpreview' ));
 		}
 		if( $this->mTitle->isProtected('edit') ) {
-			$wgOut->addHTML( '<strong>' . wfMsg( 'protectedpagewarning' ) .
-			  "</strong><br />\n" );
+			$wgOut->addWikiText( wfMsg( 'protectedpagewarning' ) );
 		}
 
 		$kblength = (int)(strlen( $this->textbox1 ) / 1024);
 		if( $kblength > 29 ) {
-			$wgOut->addHTML( '<strong>' .
-				wfMsg( 'longpagewarning', $wgLang->formatNum( $kblength ) )
-				. '</strong>' );
+			$wgOut->addWikiText( wfMsg( 'longpagewarning', $wgLang->formatNum( $kblength ) ) );
 		}
 
 		$rows = $wgUser->getOption( 'rows' );
@@ -700,12 +695,12 @@ END
 		
 		if ( $isConflict ) {
 			require_once( "DifferenceEngine.php" );
-			$wgOut->addHTML( "<h2>" . wfMsg( "yourdiff" ) . "</h2>\n" );
+			$wgOut->WikiText( '==' . wfMsg( "yourdiff" ) . '==' );
 			DifferenceEngine::showDiff( $this->textbox2, $this->textbox1,
 			  wfMsg( "yourtext" ), wfMsg( "storedversion" ) );
 
-			$wgOut->addHTML( "<h2>" . wfMsg( "yourtext" ) . "</h2>
-<textarea tabindex=6 id='wpTextbox2' name=\"wpTextbox2\" rows='{$rows}' cols='{$cols}' wrap='virtual'>"
+			$wgOut->WikiText( '==' . wfMsg( "yourtext" ) . '==' );
+			$wgOut->addHTML( "<textarea tabindex=6 id='wpTextbox2' name=\"wpTextbox2\" rows='{$rows}' cols='{$cols}' wrap='virtual'>"
 . htmlspecialchars( $wgContLang->recodeForEdit( $this->textbox2 ) ) .
 "
 </textarea>" );

languages/Language.php
@@ -714,13 +714,13 @@ The upper text area contains the page text as it currently exists.
 Your changes are shown in the lower text area.
 You will have to merge your changes into the existing text.
 <b>Only</b> the text in the upper text area will be saved when you
-press \"Save page\".\n<p>",
+press \"Save page\".<br />",
 'yourtext'		=> 'Your text',
 'storedversion' => 'Stored version',
 'nonunicodebrowser' => "<strong>WARNING: Your browser is not unicode compliant, please change it before editing an article.</strong>",
 'editingold'	=> "<strong>WARNING: You are editing an out-of-date
 revision of this page.
-If you save it, any changes made since this revision will be lost.</strong>\n",
+If you save it, any changes made since this revision will be lost.</strong>",
 'yourdiff'		=> 'Differences',
 'copyrightwarning' => "Please note that all contributions to {{SITENAME}} are
 considered to be released under the $2 (see $1 for details).
@@ -735,16 +735,13 @@ If you don't want your writing to be edited mercilessly, then don't submit it he
 You are also promising us that you wrote this yourself, or copied it from a
 public domain or similar free resource (see $1 for details).
 <strong>DO NOT SUBMIT COPYRIGHTED WORK WITHOUT PERMISSION!</strong>",
-'longpagewarning' => "WARNING: This page is $1 kilobytes long; some
+'longpagewarning' => "<strong>WARNING: This page is $1 kilobytes long; some
 browsers may have problems editing pages approaching or longer than 32kb.
-Please consider breaking the page into smaller sections.",
-'readonlywarning' => 'WARNING: The database has been locked for maintenance,
+Please consider breaking the page into smaller sections.</strong>",
+'readonlywarning' => '<strong>WARNING: The database has been locked for maintenance,
 so you will not be able to save your edits right now. You may wish to cut-n-paste
-the text into a text file and save it for later.',
-'protectedpagewarning' => "WARNING:  This page has been locked so that only
-users with sysop privileges can edit it. Be sure you are following the
-<a href='$wgScript/Project:Protected_page_guidelines'>protected page
-guidelines</a>.",
+the text into a text file and save it for later.</strong>',
+'protectedpagewarning' => "<strong>WARNING:  This page has been locked so that only users with sysop privileges can edit it. Be sure you are following the [[Project:Protected_page_guidelines|protected page guidelines]].</strong>",
 'templatesused'	=> 'Templates used on this page:',
 
 # History pages

languages/LanguageAf.php
@@ -328,11 +328,11 @@ Indien jy per ongeluk hier is, gebruik jou blaaier se '''terug''' knop.",
 Die boonste invoerboks het die teks wat tans bestaan.
 Jou wysigings word in die onderste invoerboks gewys.
 Jy sal jou wysigings moet saamsmelt met die huidige teks.
-<strong>Slegs</strong> die teks in die boonste invoerboks sal gestoor word wanneer jy \"Stoor bladsy\" druk.\n<p>",
+<strong>Slegs</strong> die teks in die boonste invoerboks sal gestoor word wanneer jy \"Stoor bladsy\" druk.<br />",
 "yourtext"		=> "Jou teks",
 "storedversion" => "Gestoorde weergawe",
 "editingold"	=> "<strong>Waarskuwing: jy is besig om 'n ou weergawe van hierdie bladsy te wysig.
-As jy dit stoor, sal enige wysigings sedert hierdie wysiging verloor word.</strong>\n",
+As jy dit stoor, sal enige wysigings sedert hierdie wysiging verloor word.</strong>",
 "yourdiff"		=> "Wysigings",
 "copyrightwarning" => "Alle bydraes aan {{SITENAME}} word beskou as beskikbaar gestel onder 
 die ''GNU Free Documentation License'' (lees $1 vir meer inligting).

languages/LanguageBg.php
@@ -556,12 +556,12 @@ MySQL дава грешка \"$3: $4\".\n",
 'editingcomment'    => "Редактиране на $1 (коментар)",
 'editconflict'  => "Различна редакция: $1",
 'explainconflict' => "Някой друг вече е променил тази страница, откакто започнахте да я редактирате.
-Горната текстова кутия съдържа текущия текст на страницата без Вашите промени, които са показани в долната кутия. За да бъдат и те съхранени, е необходимо ръчно да ги преместите в горното поле, тъй като <b>единствено</b> текстът в него ще бъде съхранен при натискането на бутона \"Съхранение\".\n<p>",
+Горната текстова кутия съдържа текущия текст на страницата без Вашите промени, които са показани в долната кутия. За да бъдат и те съхранени, е необходимо ръчно да ги преместите в горното поле, тъй като <b>единствено</b> текстът в него ще бъде съхранен при натискането на бутона \"Съхранение\".<br />",
 "yourtext"    => "Вашият текст",
 "storedversion" => "Съхранена версия",
 "editingold"  => "<strong>ВНИМАНИЕ: Редактирате остаряла
 версия на страницата.
-Ако съхраните, всякакви промени, направени след тази версия, ще бъдат изгубени.</strong>\n",
+Ако съхраните, всякакви промени, направени след тази версия, ще бъдат изгубени.</strong>",
 "yourdiff"    => "Разлики",
 # REPLACE THE COPYRIGHT WARNING IF YOUR SITE ISN'T GFDL!
 "copyrightwarning" => "<div style=\"border:1px solid #333; padding:0.5em\">
@@ -578,17 +578,17 @@ MySQL дава грешка \"$3: $4\".\n",
 Ако сте ползвали чужди материали, за които имате разрешение, непременно посочете източника.
 <strong>НЕ ПУБЛИКУВАЙТЕ ПРОИЗВЕДЕНИЯ С АВТОРСКИ ПРАВА БЕЗ РАЗРЕШЕНИЕ!</strong>
 </div>",
-"longpagewarning" => "ВНИМАНИЕ: Страницата има размер $1 килобайта; някои
+"longpagewarning" => "<strong>ВНИМАНИЕ: Страницата има размер $1 килобайта; някои
 браузъри могат да имат проблеми при редактиране на страници
 по-големи от 32Кб.
-Моля, обмислете дали страницата не може да се раздели на няколко по-малки части.",
-"readonlywarning" => "ВНИМАНИЕ: Базата данни бе затворена за поддръжка,
+Моля, обмислете дали страницата не може да се раздели на няколко по-малки части.</strong>",
+"readonlywarning" => "<strong>ВНИМАНИЕ: Базата данни бе затворена за поддръжка,
 затова в момента Вашите промени не мога да бъдат съхранени.
 Ако желаете, може да съхраните страницата като текстов файл и да се
-опитате да я публикувате по-късно.",
-"protectedpagewarning" => "ВНИМАНИЕ: Страницата е затворена за промени, само
+опитате да я публикувате по-късно.</strong>",
+"protectedpagewarning" => "<strong>ВНИМАНИЕ: Страницата е затворена за промени, само
 администратори могат да я редактират.
-Моля, следвайте [[{{ns:4}}:Защитена_страница|указанията за защитена страница]].",
+Моля, следвайте [[Project:Защитена_страница|указанията за защитена страница]].</strong>",
 
 # History pages
 #

languages/LanguageCa.php
@@ -347,11 +347,11 @@ d'edició superior tal i com apareixerà si escolliu gravar.",
 L'ària de text superior conté el text de la pàgina com existeix actualment. Els vostres canvis es mostren a l'ària de text inferior.
 Haureu d'incorporar els vostres canvis en el text existent.
 <b>Sólo</b> el text a l'ària de text superior serà gravat quan premeu
- \"Desa pàgina\".\n<p>",
+ \"Desa pàgina\".<br />",
 "yourtext"		=> "El vostre text",
 "storedversion" => "Versió emmagatzemada",
 "editingold"	=> "<strong>ATENCIÓ:Esteu editant una versió antiga d'aquesta pàgina.
-Si la graveu, els canvis fets des d'eixa revisió es perdran.</strong>\n",
+Si la graveu, els canvis fets des d'eixa revisió es perdran.</strong>",
 "yourdiff"		=> "Diferències",
 "copyrightwarning" => "Noteu que totes les contribucions a la {{SITENAME}} 
 es consideren fetes públiques sota la llicència de documentació lliure GNU 

languages/LanguageCs.php
@@ -315,10 +315,10 @@ která popsala příčinu takto:<br /><p>Máte právo se spojit se správcem sys
 "editing"		=> "Redakce stránky $1",
 "editconflict"	=> "Redakční konflikt: ",
 "explainconflict" => "Někdo změnil stránku poté, co jste ji napsal(a). Výše vidíte aktuální text článku. Vaše změny jsou uvedeny dole. Musíte sloučit své změny se stávajícím článkem.
-<b>Poue</b> výše uvedený text zůstane uchováná po kliknutí na  \"Uložit\".\n<p>" , #FIXME - double-check that this makes sense
+<b>Poue</b> výše uvedený text zůstane uchováná po kliknutí na  \"Uložit\".<br />" , #FIXME - double-check that this makes sense
 "yourtext"		=> "Váš text",
 "storedversion" => "Registrovaná verze",
-"editingold"	=> "<strong>VAROVÁNÍ: nyní redigujete starou verzi tohoto článku. Když ji uložíte, všechny změny provedené po zmíněné revizi se ztratí.</strong>\n",
+"editingold"	=> "<strong>VAROVÁNÍ: nyní redigujete starou verzi tohoto článku. Když ji uložíte, všechny změny provedené po zmíněné revizi se ztratí.</strong>",
 "yourdiff"		=> "Rozdíly",
 "copyrightwarning" => "Pamatujte, že každý příspěvek pro Wikipedii je považován jako zveřejněný dle <i>GNU Free Documentation License</i> (viz ). Pokud chcete, aby váš příspěvek nebyl redigován a rozšiřován, ne klikejte na \"Uložit\". Zároveň tím přísaháte, že příspěvek je vaším dílem nebo jste ho opsal(a) ze zdroje, na který se nevztahuje copyright.. <strong>NE POUŹÍVEJTE BEZ DOVOLENÍ DÍLA VÁZANÁ COPYRIGHTem!</strong>",
 

languages/LanguageCy.php
@@ -443,23 +443,23 @@ Os ydych yma trwy camgymeriad, cliciwch eich botwm '''nol'''.",
 "editconflict" => "Croestynnu golygyddol: $1",
 "explainconflict" => "Mae rhywun arall wedi newid y tudalen hon ers i chi dechrau golygu hi. Mae'r ardal testun uchaf yn cynnwys testun y tudalen fel y mae hi rwan. Mae eich newidiadau yn ddangos yn yr ardal testun isaf.
 Fydd rhaid i chi ymsoddi eich newidiadau i mewn i'r testun sydd mewn bod.
-<b>Dim ond</b> y testun yn yr ardal testun uchaf fydd yn cael eu cadw pan rydwch yn gwasgu \"Cadw tudalen\".\n<p>",
+<b>Dim ond</b> y testun yn yr ardal testun uchaf fydd yn cael eu cadw pan rydwch yn gwasgu \"Cadw tudalen\".<br />",
 "yourtext" => "Eich testun",
 "storedversion" => "Fersiwn wedi cadw",
-"editingold" => "<strong>RHYBUDD: Rydych yn golygu hen fersiwn y tudalen hon. Os cadwch hi, fydd unrhyw newidiadau hwyrach yn gael eu colli!.</strong>\n",
+"editingold" => "<strong>RHYBUDD: Rydych yn golygu hen fersiwn y tudalen hon. Os cadwch hi, fydd unrhyw newidiadau hwyrach yn gael eu colli!.</strong>",
 "yourdiff" => "Gwahaniaethau",
 "copyrightwarning" => "Sylwch mae pob cyfraniad i Wicipedia yn gael eu rhyddhau o dan termau'r Trwydded Dogfen Rhydd GNU (gwelwch $1 am manylion).
 Os nid ydych chi'n fodlon cael eich ysgrifeniad eu golygu heb trugaredd, a creu copïau dros y wê, peidiwch cyfranu'r ysgrifeniad yma.<br />
 Hefyd, rydych chi'n addo chi yw'r awdur y cyfraniad, neu rydych chi wedi copïo fe oddiwrth y parth cyhoeddus neu rhyw modd rhydd tebyg.
 <strong>PEIDIWCH CYFRANNU GWAITH O DAN HAWLFRAINT HEB CANIATÂD!</strong>",
-"longpagewarning" => "RHYBUDD: Mae hyd y tudalen hon yn $1 kilobyte; mae rhai porwyr yn cael problemau yn golygu tudalennau hirach na 32kb.<br />
-Ystyriwch torri'r tudalen i mewn i ddarnau llai, os gwelwch yn dda.",
-"readonlywarning" => "RHYBUDD: Mae'r databas wedi cloi i gael eu trwsio,
+"longpagewarning" => "<strong>RHYBUDD: Mae hyd y tudalen hon yn $1 kilobyte; mae rhai porwyr yn cael problemau yn golygu tudalennau hirach na 32kb.<br />
+Ystyriwch torri'r tudalen i mewn i ddarnau llai, os gwelwch yn dda.</strong>",
+"readonlywarning" => "<strong>RHYBUDD: Mae'r databas wedi cloi i gael eu trwsio,
 felly fyddwch chi ddim yn medru cadw eich olygiadau rwan. Efalle fyddwch chi'n eisio tori-a-pastio'r
-testun i mewn i ffeil testun, a cadw hi tan hwyrach.",
-"protectedpagewarning" => "RHYBUDD:  Mae tudalen hon wedi eu gloi -- dim ond defnyddwyr
+testun i mewn i ffeil testun, a cadw hi tan hwyrach.</strong>",
+"protectedpagewarning" => "<strong>RHYBUDD:  Mae tudalen hon wedi eu gloi -- dim ond defnyddwyr
 gyda braintiau 'sysop' sy'n medru eu olygu. Byddwch yn siwr rydych yn dilyn y
-<a href='$wgScriptPath/$wgMetaNamespace:Protected_page_guidelines'>gwifrau tywys tudalen amddiffyn</a>.",
+[[Project:Protected_page_guidelines|gwifrau tywys tudalen amddiffyn]].</strong>",
 
 # History pages
 #

languages/LanguageDa.php
@@ -431,13 +431,13 @@ Den øverste tekstboks indeholder den nuværende tekst.
 Dine ændringer er vist i den nederste tekstboks.
 Du er nødt til at sammenflette dine ændringer med den eksisterende tekst.
 <b>Kun</b> teksten i den øverste tekstboks vil blive gemt når du
-trykker \"Gem side\".\n<p>",
+trykker \"Gem side\".<br />",
 "yourtext"		=> "Din tekst",
 "storedversion" => "Den gemte version",
 "editingold"	=> "<strong>ADVARSEL: Du redigerer en gammel version
 af denne side.
 Hvis du gemmer den, vil alle ændringer foretaget siden denne revision blive 
-overskrevet.</strong>\n",
+overskrevet.</strong>",
 "yourdiff"		=> "Forskelle",
 "copyrightwarning" => "Læg mærke til at alle bidrag til Wikipedia er
 at betragte som udgivet under GNU Free Documentation License
@@ -448,16 +448,13 @@ Du lover os også, at du skrev teksten selv, kopierede fra en
 public domain eller lignende fri ressource.
 <strong>LÆG ALDRIG MATERIALE HER SOM ER BESKYTTET AF ANDRES OPHAVSRET UDEN 
 DERES TILLADELSE!</strong>",
-"longpagewarning" => "ADVARSEL: Denne side er $1 kilobytes lang; nogle
+"longpagewarning" => "<strong>ADVARSEL: Denne side er $1 kilobytes lang; nogle
 browsere kan have problemer med at redigerer sider der nærmer sig eller 
-er længere end 32kb. Overvej om ikke siden kan deles op i mindre dele.",
-"readonlywarning" => "ADVARSEL: Databasen er låst på grund af vedligeholdelse,
+er længere end 32kb. Overvej om ikke siden kan deles op i mindre dele.</strong>",
+"readonlywarning" => "<strong>ADVARSEL: Databasen er låst på grund af vedligeholdelse,
 så du kan ikke gemme dine ændringer lige nu. Det kan godt være en god ide at 
-kopiere din tekst til en tekstfil, så du kan gemme den til senere.",
-"protectedpagewarning" => "ADVARSEL: Denne side er låst, så kun administratorer
-kan redigere den. Sørg for at du følger 
-<a href='$wgScriptPath/$wgMetaNamespace:Politik_for_beskyttede_sider'>politiken for 
-beskyttede sider</a>.",
+kopiere din tekst til en tekstfil, så du kan gemme den til senere.</strong>",
+"protectedpagewarning" => "<strong>ADVARSEL: Denne side er låst, så kun administratorer kan redigere den. Sørg for at du følger [[Project:Politik_for_beskyttede_sider|politiken for beskyttede sider]].</strong>",
 
 # History pages
 #

commit 018241e1228b8c117e33364382c3d76dd2364b0f

Remove section edit links in edit conflict form pointing nowhere useful and only confusing people. Apparently this is a regression from r8855 (!) that went unnoticed for nearly 4.5 years. Hereby nominated for the Most Hilarious Bugs of All Time contest
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@58362 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

RELEASE-NOTES
@@ -609,6 +609,7 @@ it from source control: http://www.mediawiki.org/wiki/Download_from_SVN
   can be used to get an error message string
 * The error message shown in Special:ChangePassword now parses wiki markup
 * (bug 19859) Removed experimental HTMLDiff feature
+* Removed section edit links in edit conflict form
 
 == API changes in 1.16 ==
 

includes/EditPage.php
@@ -1601,13 +1601,13 @@ END
 );
 
 		if ( $this->isConflict && wfRunHooks( 'EditPageBeforeConflictDiff', array( &$this, &$wgOut ) ) ) {
-			$wgOut->wrapWikiMsg( '==$1==', "yourdiff" );
+			$wgOut->wrapWikiMsg( '<h2>$1</h2>', "yourdiff" );
 
 			$de = new DifferenceEngine( $this->mTitle );
 			$de->setText( $this->textbox2, $this->textbox1 );
 			$de->showDiff( wfMsg( "yourtext" ), wfMsg( "storedversion" ) );
 
-			$wgOut->wrapWikiMsg( '==$1==', "yourtext" );
+			$wgOut->wrapWikiMsg( '<h2>$1</h2>', "yourtext" );
 			$this->showTextbox2();
 		}
 		$wgOut->addHTML( $this->editFormTextBottom );