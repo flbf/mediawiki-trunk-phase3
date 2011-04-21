flbf/mediawiki-trunk-phase3

commit 4b1664e17063000d7ea9a44bbc8fe8294a5fc545

Start another attack on raw sql queries
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@80856 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/search/SearchMySQL.php
@@ -196,7 +196,7 @@ class SearchMySQL extends SearchEngine {
 		if( $this->showRedirects ) {
 			return '';
 		} else {
-			return 'AND page_is_redirect=0';
+			return 'page_is_redirect=0';
 		}
 	}
 
@@ -212,7 +212,7 @@ class SearchMySQL extends SearchEngine {
 		} else {
 			$namespaces = $this->db->makeList( $this->namespaces );
 		}
-		return 'AND page_namespace IN (' . $namespaces . ')';
+		return 'page_namespace IN (' . $namespaces . ')';
 	}
 
 	/**
@@ -240,8 +240,8 @@ class SearchMySQL extends SearchEngine {
 	 */
 	function getQuery( $filteredTerm, $fulltext ) {
 		return $this->queryMain( $filteredTerm, $fulltext ) . ' ' .
-			$this->queryRedirect() . ' ' .
-			$this->queryNamespaces() . ' ' .
+			'AND ' . $this->queryRedirect() . ' ' .
+			'AND ' . $this->queryNamespaces() . ' ' .
 			$this->queryRanking( $filteredTerm, $fulltext ) . ' ' .
 			$this->queryLimit();
 	}
@@ -276,13 +276,15 @@ class SearchMySQL extends SearchEngine {
 
 	function getCountQuery( $filteredTerm, $fulltext ) {
 		$match = $this->parseQuery( $filteredTerm, $fulltext );
-		$page        = $this->db->tableName( 'page' );
-		$searchindex = $this->db->tableName( 'searchindex' );
-		return "SELECT COUNT(*) AS c " .
-			"FROM $page,$searchindex " .
-			'WHERE page_id=si_page AND ' . $match .
-			$this->queryRedirect() . ' ' .
-			$this->queryNamespaces();
+
+		return $this->db->selectSQLText( array( 'page', 'searchindex' ),
+			'COUNT(*) AS c',
+			array(
+				'page_id=si_page',
+				$match,
+				$this->queryRedirect(),
+				$this->queryNamespaces()
+			) );
 	}
 
 	/**

commit 954141da697df87b4162060bd91cd1a9eaf6a893

Fixed SearchMySQL broken by r80856
git-svn-id: svn+ssh://svn.wikimedia.org/svnroot/mediawiki/trunk/phase3@86595 dd0e9695-b195-4be7-bd10-2dea1a65a6b6

includes/search/SearchMySQL.php
@@ -290,14 +290,18 @@ class SearchMySQL extends SearchEngine {
 	function getCountQuery( $filteredTerm, $fulltext ) {
 		$match = $this->parseQuery( $filteredTerm, $fulltext );
 
+		$redir = $this->queryRedirect();
+		$namespaces = $this->queryNamespaces();
+		$conditions = array( 'page_id=si_page', $match );
+		if ( $redir !== '' ) {
+			$conditions[] = $redir;
+		}
+		if ( $namespaces !== '' ) {
+			$conditions[] = $namespaces;
+		}
 		return $this->db->selectSQLText( array( 'page', 'searchindex' ),
 			'COUNT(*) AS c',
-			array(
-				'page_id=si_page',
-				$match,
-				$this->queryRedirect(),
-				$this->queryNamespaces()
-			),
+			$conditions,
 			__METHOD__
 		);
 	}